cf843209029c40e7ace2ac0a272ec5c813fdbe6c
mrizqiramahdhan <34591198+mrizqiramahdhan@users.noreply.github.com>
2018-04-06 17:43:21 +0800

    Membuat Atribut dan Behavior

--- src/praktek01/CPU.java
+++ src/praktek01/CPU.java
@@ -10,5 +10,17 @@ package praktek01;
  * @author WUMBO
  */
 public class CPU {
+    String Merk;
+    String Processor;
+    String RAM;
+    String HDD;
+    String PSU;
     
+    void cetakInfo() {
+        System.out.println("Merk :"+Merk);
+        System.out.println("Processor :"+Processor);
+        System.out.println("RAM :"+RAM);
+        System.out.println("HDD :"+HDD);
+        System.out.println("PSU :"+PSU);
+    }
 }
